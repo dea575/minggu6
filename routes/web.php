5995e8bacae440767c09a517c6ff8f7b01f0c7e5
Dea <deavernanda10@gmail.com>
2021-10-26 20:06:00 +0700

    tambah kolom dan menu

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,7 @@ use App\Http\Controllers\StudentController;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::resource('students', StudentController::class);
